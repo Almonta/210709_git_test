51193c28e28ab7733d90e6f5279b770692a615ce
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-09 14:02:29 +0900

    完成

--- sample.rb
+++ sample.rb
@@ -7,7 +7,7 @@ require 'active_support/time'
 
 module Clockwork
   handler do |job|
-    sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+    sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/')
     # sample.notify ("#{job}")
     sample.notify ("hello")
   end
